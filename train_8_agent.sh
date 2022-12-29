287783b2f61d006f31785a9b6f135f661defc186
gauthierboeshertz <gauthierboeshertz@gmail.com>
2022-12-29 18:05:29 +0100

    as

--- train_8_agent.sh
+++ train_8_agent.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for zeed in {1..8}
+for zeed in {1..3}
 do
    sbatch  -n 5  --cpus-per-task=1 --gpus=1  --time=24:00:00   --mem-per-cpu=40G --wrap="python3 -m scripts.train_batch_agent 'seed=${zeed}'"
     echo "Sleeping for 200 seconds"
